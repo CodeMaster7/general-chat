497d9ca05ebab695d93e26823e20a76e007eba36
sam <sammybonfanti@gmail.com>
2020-10-26 20:09:07 -0600

    real-time listener to the database added

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,4 +1,4 @@
-// adding new chat documents
+// adding new chat documents ✔️
 // setting up a real-time listener to get new chats
 // updating the username
 // updating the room
@@ -9,7 +9,7 @@ class Chatroom {
         this.username = username
         this.chats = db.collection('chats')
     }
-
+    // Methods
     async addChat(message) {
         // format a chat object to send to the database
         const now = new Date()
@@ -24,10 +24,21 @@ class Chatroom {
         const response = await this.chats.add(chat)
         return response
     }
+    getChats(callback) {
+        this.chats
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        // update the ui
+                        callback(change.doc.data()) // this invokes the function argument inside getChats on line 42
+                    }
+                })
+            })
+    }
 }
 
 const chatroom = new Chatroom('gaming', 'sam')
 
-chatroom.addChat('hello everyone')
-    .then(() => console.log('chat added'))
-    .catch(err => console.log(err))
+chatroom.getChats((data) => { // this gets invoked from the callback above
+    console.log(data);
+})
