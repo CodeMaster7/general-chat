cc346ace63c2be3bbe29c438f6a31dfed187c43f
sam <sammybonfanti@gmail.com>
2020-10-27 13:22:10 -0600

    updating username and room

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
         this.room = room
         this.username = username
         this.chats = db.collection('chats')
+        this.unsub
     }
     // Methods
     async addChat(message) {
@@ -25,9 +26,9 @@ class Chatroom {
         return response
     }
     getChats(callback) {
-        this.chats
+        this.unsub = this.chats // this returns a function 
             .where('room', '==', this.room) // get docuents from a specific collection where a certain condition is true // 1st property name we want to access
-            .orderBy('created_at') // what property you want to order by // error but click on link and index in firebase 
+            .orderBy('created_at') // what property you want to order by // error but click on link and index in firebase
             .onSnapshot(snapshot => {
                 snapshot.docChanges().forEach(change => {
                     if (change.type === 'added') {
@@ -37,6 +38,16 @@ class Chatroom {
                 })
             })
     }
+    updateName(username) {
+        this.username = username
+    }
+    updateRoom(room) {
+        this.room = room
+        console.log('room updated');
+        if (this.unsub) {
+            this.unsub()
+        }
+    }
 }
 
 const chatroom = new Chatroom('general', 'sam')
@@ -44,3 +55,12 @@ const chatroom = new Chatroom('general', 'sam')
 chatroom.getChats((data) => { // this gets invoked from the callback above
     console.log(data);
 })
+
+setTimeout(() => {
+    chatroom.updateRoom('gaming')
+    chatroom.updateName('yoshi')
+    chatroom.getChats((data) => { // this gets invoked from the callback above
+        console.log(data);
+    })
+    chatroom.addChat('hello')
+}, 3000)
